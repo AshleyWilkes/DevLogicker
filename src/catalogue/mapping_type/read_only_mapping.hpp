1703c0d3b545e48c883d739d9323f3cd20b74ef9
Ashley Wilkes <janonderek@gmail.com>
2019-10-30 11:55:55 +0100

    Commit 29.10.

--- src/catalogue/mapping_type/read_only_mapping.hpp
+++ src/catalogue/mapping_type/read_only_mapping.hpp
@@ -1,6 +1,23 @@
 #pragma once
 #include "core/grid/mapping_id.hpp"
 
+//odhalen novy, moderni pristup k MappingTypu:
+//vsechny MappingTypy<K, V> jsou v dusledku mapy
+//nejakeho typu <K, SomethingOf<V>>
+//tedy vsechny jsou mapy a klice ve vsech jsou 
+//primo typ K, takto klic Mappingu
+//rozdil je jen v tom, jak pracuji s hodnotami
+//typu V
+//
+//uz jmeno Mapping je asi divny
+//mame Scalar, kterej neobsahuje zadnej klic, jenom V
+//
+//a SomethingOf<V> si taky zaslouzi poradnejsi nazev
+//
+//ok, takze nova generace pracuje s pojmem ManagedValue<V>
+//Scalar je totez, co ManagedValue
+//dale mame pojem ManagedMap<K, V>, coz je futral nad 
+//std::map<K, ManagedValue<V>>
 namespace logicker::mapping {
 
 //??YAGNI: swap??
--- src/core/grid/grid.hpp
+++ src/core/grid/grid.hpp
@@ -1,13 +1,31 @@
 #pragma once
 
-//chtelo by to mit jmena pro nekolik stupnu na ceste od MappingId k full
-//instantiated Gridu:
+//chtelo by to mit jmena pro nekolik stupnu na ceste od ManagedValueId
+//k full instantiated Gridu:
 //
-//MappingIdSet -- mnozina MappingId
-//Mapping -- dvojice <MappingType, MappingId>
-//GridType -- mnozina Mappingu: 
-//    muze vzniknout jako Set Mappingu anebo jako dvojice <MappingType, MappingIdSet>
-//Grid -- objekt, ktery opravdu ma vsechny instance mappingu popsane v mnozine GridType
+//ManagedValueId -- id, oznacujici 1 managed hodnotu: [jmeno, typHodnoty]
+//ManagedMapId -- id, oznacujici mapu managed hodnot: [jmeno, typKlice, typHodnoty...]
+//ManagedIdsSet -- mnozina, jejimiz prvky jsou ManagedValueIds a ManagedMapIds
+//
+//ManagementType -- zpusob, jak 1 hodnotu managuju; zda se, ze (na sebe kolme) moznosti jsou 
+//    jednoduse store hodnoty VS domaina, z niz lze vyskrtavat
+//    optional (muze hodnotu nemit) VS obligatory (musi mit hodnotu, jinak error)
+//
+//ManagedValueType -- dvojice [typHodnoty, ManagementType]
+//ManagedMapType -- dvojice [typKlice, ManagedValueType...]
+//
+//ManagedValueSlot -- kompatibilni dvojice [ManagedValueId, ManagedValueType], 
+//      jinak tez dvojice [jmeno, ManagedValueType],
+//      jinak tez trojice [jmeno, typHodnoty, ManagementType]
+//ManagedMapSlot -- kompatibilni dvojice [ManagedMapId, ManagedMapType],
+//      jinak tez dvojice [jmeno, ManagedValueType...]
+//
+//ManagedSlotsSet -- mnozina, jejimiz prvky jsou ManagedValueSlots a ManagedMapSlots
+//
+//ManagedXXXType je instancializovatelny na ManagedXXX
+//
+//Grid je instancializovany ManagedSlotsSet v tom smyslu, ze kazdemu ManagedXXXSlotu je prirazena jedna instance ManagedXXX
+
 namespace logicker::core {
 
 template<template<typename...> typename MappingType, typename GridType>
