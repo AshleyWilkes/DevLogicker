8236f25554eb1798d46a6796680a0ec01625a07f
Ashley Wilkes <janonderek@gmail.com>
2019-11-13 19:12:32 +0100

    Commit 13.11. (Part #1, nastrel impl registry).

--- src/core/implementations_registry.hpp
+++ src/core/implementations_registry.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+namespace logicker::core::implementations_registry {
+
 //takze co?
 //chci poskytovat rozhrani pro kolekce objektu, implementujicich urcitou Operation nebo Relation
 //implementace je classa, ktera si sama urcuje, jakou hlavicku ma jeji vypocetni metoda
@@ -11,6 +13,11 @@
 //!!!!resp. ta metoda v predchozim radku muze vracet ValueSet<OutT>, neni vyreseno!!!!
 //to totiz neni treba pro InitStepy, takze udelejme napred tyto a zkusenejsi pristupme k reseni
 //Constraintu. Sounds good.
+//!!!!typ OutputT si musi urcit bud registry sama nebo template paramter jeji perform() metody!!!!
+//neni to vlastnost Operationy, jelikoz ValueSet<OutT> viz vyse
+//zakladni implementace podporuje jednoznacne (by registry) specifikovany OutT, usoudim-li pro potreby
+//  Constraint solvera, ze je uzitecne mit jeden objekt, podporujici vice OutT (pomoci templatovane)
+//  perform() metody), udelam wrapper nad nekolika temito zakladnimi implementacemi
 //
 //registry samotna ma template<typename... Args> metodu, ktera bere libovolne parametry,
 //prochazi zaregistrovane implementace a pokousi se je pouzit
@@ -23,3 +30,29 @@
 //musi to byt run-time zalezitost. 
 //
 //registry musi pri kompilaci znat vsechny implementacni classy, ktere ma brat v potaz
+//
+//takze na jedny strane mame statickou compile-time definici templatovane tridy, jejiz singletom
+//predstavuje interface pro klienty
+//a na druhy strane je vnitrek metody perform ty templatovany tridy, kterej dela ten vypocet
+//  tenhle vnitrek by nejspis mel mit precizneji definovana pravidla fungovani, coz by umoznilo
+//  dost mozna i relativne snadno navrhnout sirsi sadu testu
+//  pravidla jsou na druhou stranu dost easy: musi byt stejne parametru tam i tam a musi pro kazdy
+//    z nich existovat konverze zleva doprava. Pak lze vstupy registrovy perform() metody pouzit
+//    jako vstupy implovy perform() metody.
+
+template<typename Operation_, typename OutT_>
+class ImplementationsRegistry;
+
+template<typename... Implementations>
+class ImplementationsRegistryBase {
+  private:
+    ImplementationsRegistryBase() = default;
+  public:
+    ImplementationsRegistryBase(const ImplementationsRegistryBase&) = delete;
+    void operator=(const ImplementationsRegistryBase&) = delete;
+
+    template<typename... Args>
+    static int perform( const Args&... );
+};
+
+}
--- test_unit/core/implementations_registry.cpp
+++ test_unit/core/implementations_registry.cpp
@@ -1,12 +1,71 @@
 #include "gtest/gtest.h"
+#include "core/implementations_registry.hpp"
 
-//neni jasne, jak ma fungovat moznost, kdy npar. pod jednou Operation mam registrovane jak
-//metody vracejici OutT, tak metody vracejici ValueSet<OutT>. To proted neresim, at se pohnu.
-namespace {
+namespace logicker::core::implementations_registry {
+
+class DummyOperation {};
 
 //chci mit 3 implementace jedne operationy; 2 s jednim parametrem, 1 s dvema parametry; kazda necht vraci
 //  pro stejne inputy ruzny output.
+class Impl1Bool {
+  public:
+    using Implements = DummyOperation;
+    using InTs = std::tuple<bool>;
+    using OutT = int;
+    static int perform( bool /*b*/ ) { return 1; }
+};
+
+class Impl1Int {
+  public:
+    using Implements = DummyOperation;
+    using InTs = std::tuple<int>;
+    using OutT = int;
+    static int perform( int /*i*/ ) { return 2; }
+};
+
+class Impl2 {
+  public:
+    using Implements = DummyOperation;
+    using InTs = std::tuple<int, int>;
+    using OutT = int;
+    static int perform( int /*a*/, int /*b*/ ) { return 3; }
+};
+
+//???neslo by to napsat nejak pomoci XXX<DummyOperation, int, Impl1Bool, Impl1Int, Impl2>
+//nebo jeste lepe tak, aby nebylo ani treba ty prvni dva parametry specifikovat,
+//antoz ty musej bejt spolecny pro vsechny implementace, jinak je specifikace invalid a nema
+//projit kompilaci???
+//
+//tim, ze napisu XXX<DummyOperation, int, Impl1Boo, Impl1Int, Impl2> bych chtel zaridit,
+//aby zacala byt definovana specializace ImplementationRegistry<DummyOperation, int>,
+//zatimco jinak by neexistovala
+//to by asi slo, kdyby ImplementationRegistry byla alias templata do XXX, ta by pak
+//byla definovana zaroven s touto XXX. Nikoliv, to nejde, protoze XXX musi byt templatovan
+//pres vsech 5 parametru, takze ta alias templata nepujde napsat. Zatim necham takto, tj budu
+//implementace vyjmenovavat uvnitr definice classy v tuplu.
+template<>
+class ImplementationsRegistry<DummyOperation, int> :
+    public ImplementationsRegistryBase<Impl1Bool, Impl1Int, Impl2> {};
+
+}
+
+namespace {
+
+using namespace logicker::core::implementations_registry;
+
+TEST(ImplementationRegistry, Test) {
+  EXPECT_EQ( (ImplementationsRegistry<DummyOperation, int>::perform( true )), 1 );
+  EXPECT_EQ( (ImplementationsRegistry<DummyOperation, int>::perform( 1 )), 2 );
+  EXPECT_EQ( (ImplementationsRegistry<DummyOperation, int>::perform( 1, 1 )), 3 );
+  EXPECT_THROW( (ImplementationsRegistry<DummyOperation, int>::perform( true, true )), std::domain_error );
+
+}
+
 //chci zaregistrovat ty 3 operations do registry
+//  pri registraci chci ocheckovat (jinak compile-time error)
+//    ze implementace implementuje prislusnou operaci
+//    ze ma usingy InTs a OutT, kde OutT odpovida OutT typu of registry itself
+//    ze ma metodu OutT perform( InTs... )
 //chci zavolat perform metodu registry na4krat tak, aby
 //a) pouzila prvni implementaci s 1 parametrem
 //b) pouzila druhou implementaci s 1 parametrem
